a90e16c7b0f51d3ffae474dc4798c271db76a405
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-04 11:21:29 +0530

    Add files via upload

---
+++ singlearr.java
@@ -0,0 +1,24 @@
+class demo
+{
+	demo()
+	{
+		int ar[]=new int[5];
+		ar[0]=5;
+		ar[1]=10;
+		ar[2]=15;
+		ar[3]=20;
+		ar[4]=25;
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+		System.out.println(ar[3]);
+		System.out.println(ar[4]);
+	}
+}
+class singlearr
+{
+public static void main(String []args)
+{
+	demo n=new demo();
+}
+}

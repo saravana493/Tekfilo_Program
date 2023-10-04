d3934dd338cec72a75b4d2a10fda6d8b4b55b3a0
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-04 16:04:59 +0530

    Add files via upload

---
+++ cube.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class demo
+{
+	demo()
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int cube=n*n*n;
+		System.out.println(cube);
+	}
+}
+class cube
+{
+public static void main(String []args)
+{
+	demo m=new demo();
+}
+}

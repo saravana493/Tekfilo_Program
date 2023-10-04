2ddab2a978814522f859db1a39de24da86f01a9b
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-04 14:21:42 +0530

    Add files via upload

---
+++ maxinarr.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class demo
+{
+	demo()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Length of array");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		int max=arr[0];
+		for(int i=0;i<arr.length;i++)
+		{
+			
+				if(arr[i]>max)
+				{
+					max=arr[i];	
+				}
+			
+		}
+		System.out.println("Maximum Number is "+max);
+		
+	}
+}
+class maxinarr
+{
+public static void main(String []args)
+{
+	demo d=new demo();
+}
+}

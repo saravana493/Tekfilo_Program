25a71400ac8765f054592be3232ec357977b996d
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-13 13:32:27 +0530

    Add files via upload

---
+++ mathrandom.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class demo
+{
+	demo()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("The Random value is");
+		System.out.println(Math.random());
+	}
+}
+class mathrandom
+{
+	public static void main(String []args)
+	{
+		demo n=new demo();
+	}
+}

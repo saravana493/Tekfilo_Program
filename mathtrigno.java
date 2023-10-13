e05421a32b11bd740e144192467db59f836bc422
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-13 14:39:44 +0530

    Add files via upload

---
+++ mathtrigno.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class demo
+{
+	demo()
+	{
+		System.out.println(Math.cos(0));
+		System.out.println(Math.sin(Math.PI/2));
+		System.out.println(Math.tan(Math.PI/4));
+		System.out.println(Math.asin(1));
+	}
+}
+class mathtrigno
+{
+	public static void main(String []args)
+	{
+		demo n=new demo();
+	}
+}

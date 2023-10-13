ca95759946daa2e5e456a7e17e1b7cf77cd0c343
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-13 11:09:42 +0530

    Add files via upload

---
+++ arraylistfor.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class demo 
+{
+    demo()
+    {
+        ArrayList<String> n=new ArrayList<String>();
+        n.add("Saravana");
+        n.add("Mohan");
+        n.add("Suraj");
+	for(String name:n)
+	{
+        System.out.println(name);
+	}
+    }
+}
+class arraylistfor
+{
+    public static void main(String sarg[])
+    {
+
+        demo o=new demo();
+    }
+}

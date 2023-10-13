9d68bb56e64eced8fa6968723d10c58fdd4608e1
saravana493 <65144203+saravana493@users.noreply.github.com>
2023-10-13 11:13:49 +0530

    Add files via upload

---
+++ arraylistset.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class demo 
+{
+    demo()
+    {
+        ArrayList<String> s=new ArrayList<String>();
+        s.add("Saravana");
+        s.add("Mohan");
+        s.add("Suraj");
+	s.set(2,"Hari");
+        System.out.println(s);
+    }
+}
+class arraylistset
+{
+    public static void main(String sarg[])
+    {
+
+        demo o=new demo();
+    }
+}
